d38f591a74fb42379f5b0e35612ea8d3dd9e16e4
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-11-02 17:00:43 +0100

    Fixed defs

--- includes/miniRT_defs.h
+++ includes/miniRT_defs.h
@@ -1,10 +1,12 @@
+#ifndef MINIRT_DEFS_H
+# define MINIRT_DEFS_H
 
-typedef struct s_point
+typedef struct s_vector
 {
 	int	x;
-	int y;
-	int z;
-}	t_point ;
+	int	y;
+	int	z;
+}		t_vector;
 
 typedef struct s_color
 {
@@ -12,53 +14,65 @@ typedef struct s_color
 	int	g;
 	int	b;
 	int	a;
-}	t_color ;
+}		t_color;
 
 typedef struct s_camera
 {
-	t_point	center;
-	t_point	view_point;
-	t_point	n_vector;
-	int		fov;
-}	t_camera ;
+	t_vector	center;
+	t_vector	view_point;
+	t_vector	n_vector;
+	int			fov;
+}				t_camera;
 
 typedef struct s_light
 {
-	t_point	center;
-	int		brightness;
-	t_color	color;
-}	t_light ;
+	t_vector	center;
+	int			brightness;
+	t_color		color;
+}				t_light;
 
 typedef struct s_plane
 {
-	t_point	center;
-	t_point	n_vector;
-	t_color	color;
-}	t_plane ;
+	t_vector	center;
+	t_vector	n_vector;
+	t_color		color;
+}				t_plane;
 
 typedef struct s_sphere
 {
-	t_point	center;
-	t_point	n_vector;
-	int		diameter;
-	t_color	color;
-}	t_sphere ;
+	t_vector	center;
+	t_vector	n_vector;
+	int			diameter;
+	t_color		color;
+}				t_sphere;
 
 typedef struct s_cylinder
 {
-	t_point	center;
-	t_point	n_vector;
-	int		diameter;
-	int		height;
-	t_color	color;
-}	t_cylinder ;
+	t_vector	center;
+	t_vector	n_vector;
+	int			diameter;
+	int			height;
+	t_color		color;
+}				t_cylinder;
+
+typedef struct s_info
+{
+	int	ambient_light;
+	int	lights;
+	int	planes;
+	int	spheres;
+	int	cylinders;
+}		t_info;
 
 typedef struct s_data
 {
+	t_info		info;
 	t_light		ambient_light;
 	t_camera	camera;
 	t_light		*lights;
 	t_plane		*planes;
 	t_sphere	*spheres;
 	t_cylinder	*cylinders;
-}	t_data ;
+}				t_data;
+
+#endif
