5c89e9aa5650dd11a75dd974c75938312a648260
Galo joel Macias zambrano <gmacias-@car10s6.42barcelona.com>
2023-11-02 15:34:10 +0100

    autoheader

--- autoHeader.sh
+++ autoHeader.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 find . \( -name "*.c" -o -name "*.h" -o -name "Makefile" \) | while read archivo; do
-    # Comprobar si el archivo comienza con '/*'
-    if ! grep -q '^/\*' "$archivo"; then
-        # Si no comienza con '/*', ejecutar el script de expect
+    # Obtener la primera línea del archivo
+    primera_linea=$(head -n 1 "$archivo")
+
+    # Comprobar si la primera línea comienza con '/*' o '# ***'
+    if ! echo "$primera_linea" | grep -q -E '^(\/\*|# \*\*\*)'; then
+        # Si no comienza con esos patrones, ejecutar el script de expect
         ./vim_stdheader_expect.sh "$archivo"
     fi
 done
