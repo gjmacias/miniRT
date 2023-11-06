835f9379d75c1fecdcd4babc0034c144e63ba2a1
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-11-06 16:27:54 +0100

    Added definitions to header

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: gmacias- <gmacias-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:26:10 by gmacias-          #+#    #+#             */
-/*   Updated: 2023/11/02 15:26:10 by gmacias-         ###   ########.fr       */
+/*   Updated: 2023/11/06 16:26:37 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+#include "miniRT_defs.h"
+
+void	start_mlx(t_vars *vars, t_mlx_data *data);
