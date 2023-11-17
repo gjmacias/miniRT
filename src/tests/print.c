43fe42046e3ad0e7ae26a8522a40da296f7c7a56
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-11-17 16:19:10 +0100

    print update

--- src/tests/print.c
+++ src/tests/print.c
@@ -6,13 +6,20 @@
 /*   By: gmacias- <gmacias-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 13:01:50 by gmacias-          #+#    #+#             */
-/*   Updated: 2023/11/13 13:01:50 by gmacias-         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:53:32 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft_defs.h"
 #include "miniRT_defs.h"
+# define cRED "\033[0;31m"
+# define cGREEN "\033[0;32m"
+# define cYELLOW "\033[0;33m"
+# define cBLUE "\033[0;34m"
+# define cPURPLE "\033[0;35m"
+# define cCYAN "\033[0;36m"
+# define cWHITE "\033[0;37m"
 
 void	printaux(void)
 {
@@ -47,103 +54,105 @@ void	print_data(t_data *d)
 	t_sphere	*aux2;
 	t_cylinder	*aux3;
 
-	printf("Leido del archivo: %s\n", d->txt);
-	printf("lineas: %li\n", d->line);
+	printf("\nINFO\n");
+	printf("\tCamera:\t\t%li\n", d->info.camera);
+	printf("\tAmbient:\t%li\n", d->info.ambient_light);
+	printf("\tLights:\t\t%li\n", d->info.lights);
+	printf("\tPlanes:\t\t%li\n", d->info.planes);
+	printf("\tSpheres:\t%li\n", d->info.spheres);
+	printf("\tCylinders:\t%li\n", d->info.cylinders);
 	printf("\n");
-	printf("INFO : \n");
-	printf("ambient	: %li\n", d->info.ambient_light);
-	printf("camara	: %li\n", d->info.camera);
-	printf("lights	: %li\n", d->info.lights);
-	printf("planes	: %li\n", d->info.planes);
-	printf("spheres	: %li\n", d->info.spheres);
-	printf("cylinder: %li\n", d->info.cylinders);
+	printf(cRED"Camera\n"cWHITE);
+	printf("\tFOV:\t\t%.2f\n", d->camera.fov);
+	printf("\tPosition:\t[ X %.2f ]\t", d->camera.center.x);
+	printf("[ Y %.2f ]\t", d->camera.center.y);
+	printf("[ Z %.2f ]\n", d->camera.center.z);
+	printf("\tNormal:\t\t[ X %.2f ]\t", d->camera.n_vector.x);
+	printf("[ Y %.2f ]\t", d->camera.n_vector.y);
+	printf("[ Z %.2f ]\n", d->camera.n_vector.z);
 	printf("\n");
-	printf("Ambiental : \n");
-	printf("brightness: %f\n", d->ambient_light.brightness);
-	printf("colors r: %d,", d->ambient_light.color.r);
-	printf("colors g: %d,", d->ambient_light.color.g);
-	printf("colors b: %d,", d->ambient_light.color.b);
-	printf("colors a: %d\n", d->ambient_light.color.a);
+	printf(cCYAN"Ambiental\n"cWHITE);
+	printf("\tBrightness:\t%.2f\n", d->ambient_light.brightness);
+	printf("\tColor:\t\t[ R %d ]", d->ambient_light.color.r);
+	printf("[ G %d ]", d->ambient_light.color.g);
+	printf("[ B %d ]", d->ambient_light.color.b);
+	printf("[ A %d ]\n", d->ambient_light.color.a);
 	printf("\n");
-	printf("Camera : \n");
-	printf("FOV: %f\n", d->camera.fov);
-	printf("center x: %f,", d->camera.center.x);
-	printf("center y: %f,", d->camera.center.y);
-	printf("center z: %f\n", d->camera.center.z);
-	printf("n_vector x: %f,", d->camera.n_vector.x);
-	printf("n_vector y: %f,", d->camera.n_vector.y);
-	printf("n_vector z: %f\n", d->camera.n_vector.z);
-	printf("\n");
-	printf("\n");
-	printf("_____________LIST________________ : \n");
+	printf("\t-\t-\t-\t-\t-\t-\t-\t-\n");
+	printf("\t|\t\t\t  LISTS\t\t\t\t|\n");
+	printf("\t-\t-\t-\t-\t-\t-\t-\t-\n\n");
 	iter[0] = (t_list *)d->lights;
 	while (iter[0])
 	{
-		printf("Lights : \n");
+		printf(cYELLOW"Lights\n"cWHITE);
 		aux = (t_light *)iter[0]->content;
-		printf("center x: %f,", aux->center.x);
-		printf("center y: %f,", aux->center.y);
-		printf("center z: %f\n", aux->center.z);
-		printf("brightness: %f\n", aux->brightness);
-		printf("colors r: %d,", aux->color.r);
-		printf("colors g: %d,", aux->color.g);
-		printf("colors b: %d,", aux->color.b);
-		printf("colors a: %d\n", aux->color.a);
+		printf("\tBrightness:\t%.2f\n", aux->brightness);
+		printf("\tPosition:\t[ X %.2f ]\t", aux->center.x);
+		printf("[ Y %.2f ]\t", aux->center.y);
+		printf("[ Z %.2f ]\n", aux->center.z);
+		printf("\tColor:\t\t[ R %d ]", aux->color.r);
+		printf("[ G %d ]", aux->color.g);
+		printf("[ B %d ]", aux->color.b);
+		printf("[ A %d ]\n", aux->color.a);
+		printf("\n");
 		iter[0] = iter[0]->next;
 	}
-	printf("\n");
 	iter[1] = (t_list *)d->planes;
 	while (iter[1])
 	{
-		printf("Planes : \n");
+		printf(cGREEN"Planes\n"cWHITE);
 		aux1 = (t_plane *)iter[1]->content;
-		printf("center x: %f,", aux1->center.x);
-		printf("center y: %f,", aux1->center.y);
-		printf("center z: %f\n", aux1->center.z);
-		printf("n_vector x: %f,", aux1->n_vector.x);
-		printf("n_vector y: %f,", aux1->n_vector.y);
-		printf("n_vector z: %f\n", aux1->n_vector.z);
-		printf("colors r: %d,", aux1->material.color.r);
-		printf("colors g: %d,", aux1->material.color.g);
-		printf("colors b: %d,", aux1->material.color.b);
-		printf("colors a: %d\n", aux1->material.color.a);
+		printf("\tPosition:\t[ X %.2f ]\t", aux1->center.x);
+		printf("[ Y %.2f ]\t", aux1->center.y);
+		printf("[ Z %.2f ]\n", aux1->center.z);
+		printf("\tNormal:\t\t[ X %.2f ]\t", aux1->n_vector.x);
+		printf("[ Y %.2f ]\t", aux1->n_vector.y);
+		printf("[ Z %.2f ]\n", aux1->n_vector.z);
+		printf("\tColor:\t\t[ R %d ]", aux1->material.color.r);
+		printf("[ G %d ]", aux1->material.color.g);
+		printf("[ B %d ]", aux1->material.color.b);
+		printf("[ A %d ]\n", aux1->material.color.a);
+		printf("\n");
 		iter[1] = iter[1]->next;
 	}
-	printf("\n");
 	iter[2] = (t_list *)d->spheres;
 	while (iter[2])
 	{
-		printf("Spheres : \n");
+		printf(cBLUE"Spheres\n"cWHITE);
 		aux2 = (t_sphere *)iter[2]->content;
-		printf("center x: %f,", aux2->center.x);
-		printf("center y: %f,", aux2->center.y);
-		printf("center z: %f\n", aux2->center.z);
-		printf("Diameter: %f\n", aux2->diameter);
-		printf("colors r: %d,", aux2->material.color.r);
-		printf("colors g: %d,", aux2->material.color.g);
-		printf("colors b: %d,", aux2->material.color.b);
-		printf("colors a: %d\n", aux2->material.color.a);
+		printf("\tDiameter:\t%.2f\n", aux2->diameter);
+		printf("\tPosition:\t[ X %.2f ]\t", aux2->center.x);
+		printf("[ Y %.2f ]\t", aux2->center.y);
+		printf("[ Z %.2f ]\n", aux2->center.z);
+		printf("\tColor:\t\t[ R %d ]", aux2->material.color.r);
+		printf("[ G %d ]", aux2->material.color.g);
+		printf("[ B %d ]", aux2->material.color.b);
+		printf("[ A %d ]\n", aux2->material.color.a);
+		printf("\n");
 		iter[2] = iter[2]->next;
 	}
 	printf("\n");
 	iter[3] = (t_list *)d->cylinders;
 	while (iter[3])
 	{
-		printf("Cylinders : \n");
+		printf(cPURPLE"Cylinders\n"cWHITE);
 		aux3 = (t_cylinder *)iter[3]->content;
-		printf("center x: %f,", aux3->center.x);
-		printf("center y: %f,", aux3->center.y);
-		printf("center z: %f\n", aux3->center.z);
-		printf("n_vector x: %f,", aux3->n_vector.x);
-		printf("n_vector y: %f,", aux3->n_vector.y);
-		printf("n_vector z: %f\n", aux3->n_vector.z);
-		printf("Diameter: %f\n", aux3->diameter);
-		printf("Height: %f\n", aux3->height);
-		printf("colors r: %d,", aux3->material.color.r);
-		printf("colors g: %d,", aux3->material.color.g);
-		printf("colors b: %d,", aux3->material.color.b);
-		printf("colors a: %d\n", aux3->material.color.a);
+		printf("\tDiameter:\t%.2f\n", aux3->diameter);
+		printf("\tHeight:\t\t%.2f\n", aux3->height);
+		printf("\tPosition:\t[ X %.2f ]\t", aux3->center.x);
+		printf("[ Y %.2f ]\t", aux3->center.y);
+		printf("[ Z %.2f ]\n", aux3->center.z);
+		printf("\tNormal:\t\t[ X %.2f ]\t", aux3->n_vector.x);
+		printf("[ Y %.2f ]\t", aux3->n_vector.y);
+		printf("[ Z %.2f ]\n", aux3->n_vector.z);
+		printf("\tColor:\t\t[ R %d ]", aux3->material.color.r);
+		printf("[ G %d ]", aux3->material.color.g);
+		printf("[ B %d ]", aux3->material.color.b);
+		printf("[ A %d ]\n", aux3->material.color.a);
+		printf("\n");
 		iter[3] = iter[3]->next;
 	}
+	printf("Lines: %li\n", d->line);
+	printf("Filename: %s\n", d->txt);
 	printf("\n");
 }
