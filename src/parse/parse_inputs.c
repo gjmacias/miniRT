bbdfc835a6e75d1aa6908b77f6d043a8bcd05c3f
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-11-21 16:17:59 +0100

    Removed checker_normal, implemented vector_length calculator

--- src/parse/parse_inputs.c
+++ src/parse/parse_inputs.c
@@ -6,7 +6,7 @@
 /*   By: gmacias- <gmacias-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 13:01:00 by gmacias-          #+#    #+#             */
-/*   Updated: 2023/11/17 17:54:50 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/11/21 14:58:00 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ void	input_vector(char *s, t_data *p, t_vector *vector)
 		exit(EXIT_FAILURE);
 	}
 	arraytodouble(s, vector);
-	if (checker_normal(vector))
+	if (vector_length(vector) != 1)
 	{
 		ft_putstr_fd(" > Wrong values: Normal\n", 2);
 		exit(EXIT_FAILURE);	
