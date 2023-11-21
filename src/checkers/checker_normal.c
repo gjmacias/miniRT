49ff20a6c3beed8a5509e6c7e25e37efcd6e8150
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-11-21 12:14:35 +0100

    Implemented check for normal vectors

--- src/checkers/checker_normal.c
+++ src/checkers/checker_normal.c
@@ -6,21 +6,19 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 17:31:03 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/11/17 18:17:52 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/11/21 12:14:20 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT_defs.h"
+#include <math.h>
 
 int	checker_normal(t_vector *vector)
 {
 	double	n;
 
-	n = vector->x + vector->y + vector->z; // ToDo: Calcular modulo del vector
-	if (vector->x > 1 || vector->x < -1 || vector->y > 1 || vector->y < -1 
-		|| vector->z > 1 || vector->z < -1)
-		return (1);
-	else if ( n > 1 || n < 1 ) // Si el modulo del vector es diferente de 1 es incorrecto
+	n = sqrt(pow(vector->x, 2) + pow(vector->y, 2) + pow(vector->z, 2));
+	if (n != 1)
 		return (1);
 	return (0);
 }
