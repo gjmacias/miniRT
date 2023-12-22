964ae1170c69b4d6a5cdfc1f695cf62086128a1f
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-12-22 17:18:34 +0100

    Finished cylinder maybe

--- src/utils/intersections.c
+++ src/utils/intersections.c
@@ -6,13 +6,24 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:52:52 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/12/22 14:45:55 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:18:15 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include "miniRT_defs.h"
 #include <math.h>
+
+static double	quadratic_formula(double a, double b, double c);
+{
+	double	t[2];
+
+	t[0] = (-b + sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+	t[1] = (-b - sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+	if (t[0] < t[1])
+		return (t[0]);
+	return (t[1]);
+}
 
 double	rayhit_pl(t_vector *ray0, t_vector *ray_dir, t_plane *plane)
 {
@@ -46,22 +57,23 @@ double	rayhit_sp(t_vector *ray0, t_vector *ray_dir, t_sphere *sp)
 		return (-1.0);
 	t[0] = (-coef[1] + sqrt(discriminant)) / (2.0 * coef[0]);
 	t[1] = (-coef[1] - sqrt(discriminant)) / (2.0 * coef[0]);
-	if (t[0] < t[1])
+	if ((t[0] > 0 && (t[0] < t[1])) || (t[0] < 0 && (t[0] > t[1])))
 		return (t[0]);
 	return (t[1]);
 }
 
-/*
 double	rayhit_cy(t_vector *ray0, t_vector *ray_dir, t_cylinder *cy)
 {
-	A = ray0;
-	b = ray_dir;
+	double	D;
+	double	V;
+	double	X;
+	double	t;
 
-	P(t) =	A + tb;
-			p0 + vt;
-
-	((ray0->x + ray_dir->x * t) * (ray0->x + ray_dir->x * t)) + ((ray0->z + ray_dir->z * t) * (ray0->z + ray_dir->z * t)) = cy->r_sq;
-	pow(ray0->x, 2) + 2.0 * (ray0->x * ray_dir->x * t) + pow(ray_dir->x * t, 2) + pow(ray0->z, 2) + 2.0 * (ray0->z * ray_dir->z * t) + pow(ray_dir->z * t, 2) = cy->r_sq;
-	pow(ray0->x, 2) + pow(ray0->z, 2) + 2.0 * (ray0->x * ray_dir->x * t) + pow(ray_dir->x * t, 2) + 2.0 * (ray0->z * ray_dir->z * t) + pow(ray_dir->z * t, 2) = cy->r_sq;
+	D = ray_dir;
+	V = cy->n_vector;
+	X = v_subtract(ray0, cy->center);
+	t = quadratic_formula(dot_v(D, D) - pow(dot_v(D, V), 2), \
+		2 * (dot_v(D, X) - dot_v(D, V) * dot_v(X, V)), \
+		dot_v(X, X) - pow(dot_v(X, V), 2) - cy->r_sq);
+	return (t);
 }
-*/
